0ae670b78187403b72716349f2ac7f6902bd3398
faisalkoreshi <faisal.koreshi@gmail.com>
2021-10-06 22:08:18 -0400

    new added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quizData =[
 	{
-		question:'Which of the following are basic security guard duties?',
+		question:'1. Which of the following are basic security guard duties?',
 		a:'Enforcing trafing rulls on city streets',
 		b:'Patrolling the public place and enforcing law',
 		c:'Enforcing rules and regulations on private property',
@@ -8,7 +8,7 @@ const quizData =[
 		correct: 'c'
 	},
 	{
-		question:'The criminal code of Canada allows :',
+		question:'2. The criminal code of Canada allows :',
 		a:'Special authorities for police officers and security guard',
 		b:'Security guards the same authorities as other private citizens',
 		c:'Security guards to use reasonable force arresting people named on warrant ',
@@ -16,7 +16,7 @@ const quizData =[
 		correct: 'b'
 	},
 	{
-		question:'What is the best description of a site patrol duty?:',
+		question:'3. What is the best description of a site patrol duty?:',
 		a:'Assigned to a specific site. patrolling the property',
 		b:'Assigned to a specific static post',
 		c:'Assigned to patrol between many sites',
@@ -24,7 +24,7 @@ const quizData =[
 		correct: 'a'
 	},
 	{
-		question:'Which of the following regulates the security industry in Ontario?',
+		question:'4. Which of the following regulates the security industry in Ontario?',
 		a:'The criminal code and charter of right  ',
 		b:'The private security invatigative services act, 2005.',
 		c:'The police services act.',
@@ -32,13 +32,46 @@ const quizData =[
 		correct: 'b'
 	},
 	{
-		question:'What is the maximum penalty for an individuals violation of the code of conduct ?',
+		question:'5. What is the maximum penalty for an individuals violation of the code of conduct ?',
 		a:'$1000.00 fine  ',
 		b:'Community service',
 		c:'$25,000.00 fine and/or a year in jail',
 		d:'Suspension or revocation of the security gurd licence ',
 		correct: 'c'
+	},
+	{
+		question:'6. Licensees are prohibited from the following acts',
+		a:'Following someone without their  consent for the purpose of gathering information',
+		b:'Forcibly removing an unwantade patron from a night club',
+		c:'Assisting an owner of an apartment by reporting a tenants activits',
+		d:'Providing information about a person in a witness protection program',
+		correct: 'd'
+	},
+	{
+		question:'7. Which of the following are valid surveillance techniques?',
+		a:'Observing The physical environment and Situational awareness',
+		b:'Using parabolic microphone to listen in on private conversation',
+		c:'Using telescope to peek into Windows of residence',
+		d:'Purposely following A vehicle closely to intimidate The subject who is criminal',
+		correct: 'a'
+	},
+	{
+		question:'',
+		a:'',
+		b:'',
+		c:'',
+		d:'',
+		correct: 'a'
+	},
+	{
+		question:'',
+		a:'',
+		b:'',
+		c:'',
+		d:'',
+		correct: 'a'
 	}
+
 
 
 
